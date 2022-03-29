dc46aed074bfadb0b42be2e916dfc572a3eabe42
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-03-29 22:12:36 +0000

    first commit

---
+++ src/features/searchTerm/searchTermSlice.js
@@ -0,0 +1,29 @@
+
+
+export const setSearchTerm = (term) => {
+    return {
+        type: 'searchTerm/setSearchTerm',
+        payload: term
+    }
+}
+
+export const clearSearchTerm = () => {
+    return {
+        type: 'searchTerm/clearSearchTerm'
+    }
+}
+
+const initialState = ''
+export const searchTermReducer = (state = initialState, action) => {
+    switch (action.type){
+        case 'searchTerm/setSearchTerm': {
+            return action.payload;
+        }
+        case 'searchTerm/clearSearchTerm': {
+            return '';
+        }
+        default: {
+            return state;
+        }
+    }
+}
